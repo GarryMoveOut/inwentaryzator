0c0ae4c9571b826fe1b499e61f9e828e77be3f98
mwypyska <mwypyska@gmail.com>
2014-05-19 12:48:49 +0200

    Poprawki okien

--- PROJEKT/inwentaryzator/inwentaryzator/Logowanie.Designer.cs
+++ PROJEKT/inwentaryzator/inwentaryzator/Logowanie.Designer.cs
@@ -29,12 +29,10 @@
         private void InitializeComponent()
         {
             this.button_login = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.textbox_login = new System.Windows.Forms.TextBox();
             this.textbox_haslo = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
@@ -47,15 +45,6 @@
             this.button_login.Text = "Loguj";
             this.button_login.UseVisualStyleBackColor = true;
             this.button_login.Click += new System.EventHandler(this.button_login_Click);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(181, 271);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
             // 
             // textbox_login
             // 
@@ -93,15 +82,6 @@
             this.label2.TabIndex = 5;
             this.label2.Text = "Hasło:";
             // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(296, 52);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
-            this.label3.TabIndex = 6;
-            this.label3.Text = "label3";
-            // 
             // label4
             // 
             this.label4.AutoSize = true;
@@ -115,14 +95,12 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 212);
+            this.ClientSize = new System.Drawing.Size(289, 212);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textbox_haslo);
             this.Controls.Add(this.textbox_login);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.button_login);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
@@ -136,12 +114,10 @@
         #endregion
 
         private System.Windows.Forms.Button button_login;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.TextBox textbox_login;
         private System.Windows.Forms.TextBox textbox_haslo;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
     }
 }
--- PROJEKT/inwentaryzator/inwentaryzator/WgenRaport.Designer.cs
+++ PROJEKT/inwentaryzator/inwentaryzator/WgenRaport.Designer.cs
@@ -85,6 +85,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
             this.ClientSize = new System.Drawing.Size(254, 262);
             this.Controls.Add(this.but_wyloguj);
             this.Controls.Add(this.but_eksportXML);
