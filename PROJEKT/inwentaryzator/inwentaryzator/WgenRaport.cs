0458bdba123ed2455d08db95f34178d65f0e6a14
GarryMoveOut <damian.true@hotmail.com>
2014-05-31 19:45:09 +0200

    Dodanie funkcji ekportu wszystkich pracowników do xml

--- PROJEKT/inwentaryzator/inwentaryzator/WgenRaport.cs
+++ PROJEKT/inwentaryzator/inwentaryzator/WgenRaport.cs
@@ -50,16 +50,19 @@ namespace inwentaryzator
                     rap_wszystkich_produktow();
                     break;
                 case 1:
-
+                    
                     break;
                 case 2:
-
+                    rap_wszystkich_pracownikow();
                     break;
             }
                 
 
             
         }
+        //
+        //metoda do tworzenia i zapisywania raportu wszystkich produktów
+        //
         private void rap_wszystkich_produktow()
         {
             try
@@ -93,6 +96,71 @@ namespace inwentaryzator
                                 new XElement("ILOSC", produkt.Ilosc),
                                 new XElement("CENA",produkt.Cena)
                                 )
+                        )
+                    );
+
+                //Wybranie miejsca zapisania
+                string nazwa;
+                SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+                saveFileDialog1.InitialDirectory = @"C:\";              //początkowa ścieżka zapisu
+                saveFileDialog1.AddExtension = true;                    //automatyczne dodawanie rozszerzenia
+                saveFileDialog1.Title = "Eksportowanie raportu XML";    //Tytuł okna zapisu
+                //saveFileDialog1.CheckFileExists = true;
+                saveFileDialog1.CheckPathExists = true;
+                saveFileDialog1.DefaultExt = "xml";
+                saveFileDialog1.Filter = "eXtensible Markup Language (*.xml)|*.xml";
+                saveFileDialog1.FilterIndex = 1;
+                saveFileDialog1.RestoreDirectory = true;
+
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    nazwa = saveFileDialog1.FileName;
+                    raport_xml.Save(nazwa);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Błąd wczytywania plików");
+            }
+        }
+        //
+        //metoda do tworzenia i zapisywania do pliku xml wszystkich pracowników
+        //
+        private void rap_wszystkich_pracownikow()
+        {
+            try
+            {
+                //Plik z pracownikami
+                XDocument xml = XDocument.Load(@".\inwentaryzator\\data\\uzytkownicy.xml");
+
+                //Utworzenie i wczytanie do listy pracowników
+                List<cPracownik> lista_pracownikow = (
+                    from pracownik in xml.Root.Elements("UZYTKOWNIK")
+                    select new cPracownik(
+                        pracownik.Element("IMIE").Value,
+                        pracownik.Element("NAZWISKO").Value,
+                        pracownik.Attribute("LOGIN").Value,
+                        pracownik.Element("HASLO").Value,
+                        pracownik.Element("UPRAWNIENIA").Value,
+                        pracownik.Element("INFORMACJE").Value
+                        )
+                    ).ToList<cPracownik>();
+
+                //Zczytanie pracowników z listy do pliku xml
+                XDocument raport_xml = new XDocument(
+                    new XDeclaration("1.0", "utf-8", "yes"),
+                    new XComment("RAPORT: SPIS WSZYSTKICH PRACOWNIKOW"),
+                    new XElement("UZYTKOWNICY",
+                        from pracownik in lista_pracownikow
+                        orderby pracownik.login
+                        select new XElement("UZYTKOWNIK",
+                            new XAttribute("LOGIN", pracownik.login),
+                            new XElement("HASLO", pracownik.haslo),
+                            new XElement("IMIE", pracownik.imie),
+                            new XElement("NAZWISKO", pracownik.nazwisko),
+                            new XElement("UPRAWNIENIA", pracownik.uprawnienia),
+                            new XElement("INFORMACJE", pracownik.informacje)
+                            )
                         )
                     );
 
